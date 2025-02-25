b4d47be79c912bcf1dc1f86f8d57063cd4f94e7d
Yohan Markose <yohanmarkose7@gmail.com>
2025-02-25 16:12:56 -0500

    Update deploy_notebooks.sql

--- scripts/deploy_notebooks.sql
+++ scripts/deploy_notebooks.sql
@@ -12,9 +12,9 @@ Last Updated: 6/11/2024
 -- Create the Notebooks
 --USE SCHEMA {{env}}_SCHEMA;
 
-CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."{{env}}_SCHEMA"."{{env}}_01_load_files"')
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."{{env}}_{{schema}}"."{{env}}_01_load_files"')
     FROM '@"FRED_DB"."INTEGRATIONS"."FRED_GIT_REPO"/branches/"{{branch}}"/notebooks/01_load_files/'
     QUERY_WAREHOUSE = 'FRED_WH'
     MAIN_FILE = '01_load_files.ipynb';
 
-ALTER NOTEBOOK "FRED_DB"."{{env}}_SCHEMA"."{{env}}_01_load_files" ADD LIVE VERSION FROM LAST;
+ALTER NOTEBOOK "FRED_DB"."{{env}}_{{schema}}"."{{env}}_01_load_files" ADD LIVE VERSION FROM LAST;
