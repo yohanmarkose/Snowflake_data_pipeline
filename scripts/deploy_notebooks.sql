5ddde5caf43bea0b88ae93faffc3bc34e9434f07
Yohan Markose <markose.y@northeastern.edu>
2025-02-26 19:44:02 -0500

    update deploy notebook file

--- scripts/deploy_notebooks.sql
+++ scripts/deploy_notebooks.sql
@@ -25,3 +25,10 @@ CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."{{env}}_{{schema2}}"."{{env}}_
     MAIN_FILE = '02_raw_to_harmonized.ipynb';
 
 ALTER NOTEBOOK "FRED_DB"."{{env}}_{{schema2}}"."{{env}}_02_raw_to_harmonized" ADD LIVE VERSION FROM LAST;
+
+CREATE OR REPLACE NOTEBOOK IDENTIFIER('"FRED_DB"."{{env}}_{{schema3}}"."{{env}}_03_analytics_table_processing"')
+    FROM '@"FRED_DB"."INTEGRATIONS"."FRED_GIT_REPO"/branches/"{{branch}}"/notebooks/03_analytics_table_processing/'
+    QUERY_WAREHOUSE = 'FRED_WH'
+    MAIN_FILE = '03_analytics_table_processing.ipynb';
+
+ALTER NOTEBOOK "FRED_DB"."{{env}}_{{schema3}}"."{{env}}_03_analytics_table_processing" ADD LIVE VERSION FROM LAST;
